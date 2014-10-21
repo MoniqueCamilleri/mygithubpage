e0cb380c2d0ddc914891f085c04c3ca06a3a41e4
Hayden Sookchand <Haydensookchand@yahoo.com>
2014-10-21 17:35:24 +0200

    Update factory.js

--- breaking_bad/system/factory.js
+++ breaking_bad/system/factory.js
@@ -4,7 +4,7 @@ app.factory('myNotices', function($http, $q, $timeout) {
 		getMsg: function(id) {	    
 			var deferred = $q.defer();
 		//	$http.get('resources/data.json')
-		alert(id);
+		alert(url);
 		  // var url = "https://api.myjson.com/bins/3sjud.json";
 
     $http.get(url)
