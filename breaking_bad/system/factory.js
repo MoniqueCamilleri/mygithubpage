e85d7a26b85e636526a12c149d8f106ffff6e7b2
Hayden Sookchand <Haydensookchand@yahoo.com>
2014-10-21 14:10:53 +0200

    Update factory.js

--- breaking_bad/system/factory.js
+++ breaking_bad/system/factory.js
@@ -2,7 +2,8 @@ app.factory('myNotices', function($http, $q, $timeout) {
    return {
 		getMsg: function() {	    
 			var deferred = $q.defer();
-			$http.get('resources/data.json')
+		//	$http.get('resources/data.json')
+			$http.get('https://github.com/HaydenSookchand/mygithubpage/blob/gh-pages/breaking_bad/resources/data.json')
 				.success(function(data) {
 					deferred.resolve(data); //resolves the promise
 				})
