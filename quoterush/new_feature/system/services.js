6b6f0f55bfbb862f38bb66748c267c57b0601d26
Hayden Sookchand <hayden@infinitevirtuality.co.za>
2014-10-28 21:43:11 +0200

    uploading favorites feature

---
+++ quoterush/new_feature/system/services.js
@@ -0,0 +1,7 @@
+app.service('analytics', [
+  '$rootScope', '$window', '$location', function($rootScope, $window, $location) {
+    var send = function(evt, data) {
+      ga('send', evt, data);
+    }
+  }
+]);
