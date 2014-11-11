8e646839480fe13a26f9d49607c673e6c8b89b3e
Hayden Sookchand <hayden@infinitevirtuality.co.za>
2014-11-11 14:19:32 +0200

    removing files

--- quoterush/new_feature/system/services.js
+++
@@ -1,7 +0,0 @@
-app.service('analytics', [
-  '$rootScope', '$window', '$location', function($rootScope, $window, $location) {
-    var send = function(evt, data) {
-      ga('send', evt, data);
-    }
-  }
-]);
